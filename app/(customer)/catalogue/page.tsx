0d207ee68111892b3efafadea2d2c378face027d
pilexy4real556 <pilexy4real556@gmail.com>
2026-01-01 04:57:44 +0000

    Fix client directive order

--- app/(customer)/catalogue/page.tsx
+++ app/(customer)/catalogue/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useCart } from "@/app/lib/cart";
 import { useEffect, useState } from "react";
 import { collection, getDocs, query, where } from "firebase/firestore";
